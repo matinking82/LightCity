cdb48929cbc926dc6ce62ff2393211cd793bef2a
matinking82 <Matinking82@gmail.com>
2023-06-25 09:49:28 +0330

    add ShowMenu

--- src/main/java/org/example/Menu.java
+++ src/main/java/org/example/Menu.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.Utils.Console;
 import org.example.models.User;
 
 import java.util.Scanner;
@@ -7,25 +8,41 @@ import java.util.Scanner;
 public class Menu {
     private static Game game = new Game();
     private static Scanner scanner = new Scanner(System.in);
+    private static Database db = new Database();
+    public static void main(String[] args) {
+            showMenu();
+    }
     public static void showMenu(){
-       mainMenu();
-       String next = scanner.next();
-       if (next.equals("1")) {
-           game.continueGame(loginMenu());
-       }else if(next.equals("2")){
-           game.startGame(loginMenu());
-       }else if (next.equals("3")){
-           joinServer();
-       }else if (next.equals("4"))
-           System.exit(0);
+        mainMenu();
+        String next = scanner.next();
+        if (next.equals("1")) {
+            game.continueGame(loginMenu());
+        }else if(next.equals("2")){
+            game.startGame(loginMenu());
+        }else if (next.equals("3")){
+            joinServer();
+        }else if (next.equals("4"))
+            System.exit(0);
     }
     public static void mainMenu(){
-//        show menu : sout ()
+
     }
 
     public static User loginMenu(){
-//       get user info : username, password
-        return null;
+        Console.ClearConsole();
+        while (true){
+            User user = new User();
+            System.out.println("Please enter your username: ");
+            user.setUsername(scanner.next());
+            System.out.println("Please enter your Password: ");
+            user.setPassword(scanner.next());
+
+            if (db.loginGame(user)){
+                return user;
+            }else {
+                System.out.println("LoginFailed");
+            }
+        }
     }
 
     private static void joinServer(){
@@ -34,8 +51,5 @@ public class Menu {
         System.out.print("Enter Server Port :");
         int port = scanner.nextInt();
         game.joinServer(ip,port);
-    }
-    public static void main(String[] args) {
-            showMenu();
     }
 }
