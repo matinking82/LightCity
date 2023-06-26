63bb8910320fc61f30a3483a1c57d833a7c349e2
matinking82 <Matinking82@gmail.com>
2023-06-26 09:35:58 +0330

    add PlayInfo

---
+++ src/main/java/org/example/Utils/PlayInfo.java
@@ -0,0 +1,8 @@
+package org.example.Utils;
+
+import org.example.models.Character;
+
+public class PlayInfo {
+    public static float[] currentCoordinates;
+    public static Character playCharacter;
+}
