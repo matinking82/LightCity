b71b72ecdb0e4ad678af0c8d4e9231624a462d16
matinking82 <Matinking82@gmail.com>
2023-06-26 09:35:42 +0330

    add ShowMenu

--- src/main/java/org/example/Utils/Console.java
+++ src/main/java/org/example/Utils/Console.java
@@ -1,7 +1,15 @@
 package org.example.Utils;
 
+import java.util.List;
+
 public class Console {
     public static void ClearConsole(){
         //TODO
+    }
+
+    public static void ShowMenu(List<String> items){
+        for (int i =0;i<items.size();i++){
+            System.out.println(Integer.toString(i+1)+" - "+items.get(i));
+        }
     }
 }
